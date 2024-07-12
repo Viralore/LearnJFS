fc0b3faefaca446b5fc7e72b2942aa975bada8eb
Yogendra <yogendra.12109428@gmail.com>
2024-07-12 10:41:52 +0530

    added first springboot project libboot

---
+++ libboot/src/main/java/com/lpu/lib/LibbootApplication.java
@@ -0,0 +1,13 @@
+package com.lpu.lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibbootApplication.class, args);
+	}
+
+}
---
+++ libboot/src/test/java/com/lpu/lib/LibbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpu.lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
